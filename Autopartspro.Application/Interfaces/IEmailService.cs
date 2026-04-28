aea03a7c53fc2394a6fa5ba5491c0761efbc8c15
sandesh pandey <sandesh.pandey00112@gmail.com>
2026-04-28 14:28:50 +0545

    Clean tracked build files

--- Autopartspro.Application/Interfaces/IEmailService.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Autopartspro.Application.Interfaces;
-
-public interface IEmailService
-{
-    Task SendOtpEmailAsync(string toEmail, string fullName, string otpCode);
-    Task SendGenericEmailAsync(string toEmail, string subject, string htmlBody);
-} 
--- Autopartspro.Domain/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Autopartspro.Domain
-{
-    public class Class1
-    {
-
-    }
-}
